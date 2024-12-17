a80d72f6021b4344c95505947bd05a89fe33ec19
TracerStudySmekdapapa <habibiexd@gmail.com>
2024-12-17 23:46:23 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-  },
 };
 
 export default nextConfig;
